9ac8d96e8d31e45897083c4b8602ebca7c0dd8b2
deepak <96181928+logsbydeepak@users.noreply.github.com>
2025-02-15 11:08:22 +0530

    add bubbletea and log

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+)
 
 func main() {
+	m := model{}
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Error("Error while starting program", err)
+		os.Exit(1)
+	}
 	fmt.Println("snake-shell")
+}
+
+type model struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return "snake-shell"
 }
